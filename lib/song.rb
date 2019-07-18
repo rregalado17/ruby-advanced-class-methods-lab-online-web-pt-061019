7107895f7dc96fbb8c7bcc429137007f8e7998ff
Robinson Regalado <rregalado17@gmail.com>
2019-07-18 19:38:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,14 @@ class Song
     song 
   end
 
-
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song 
+  end 
+  
 
 
 
